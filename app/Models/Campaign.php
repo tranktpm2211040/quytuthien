d1b2422f14f86a6c1cc2c35793ce71073d37215a
tranktpm2211040 <nhtranktpm2211040@student.ctuet.edu.vn>
2026-03-27 01:42:49 +0700

    Campaigns

---
+++ app/Models/Campaign.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Campaign extends Model
+{
+    use HasFactory;
+
+    // Cho phép thêm dữ liệu vào các cột này
+    protected $fillable = [
+        'contract_address',
+        'title',
+        'description',
+        'image_url',
+        'start_date',
+        'end_date',
+        'status',
+    ];
+}
