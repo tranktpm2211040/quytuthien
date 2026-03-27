bf0ff2fd316e17aa51f33212a3be489376c7e1f8
tranktpm2211040 <nhtranktpm2211040@student.ctuet.edu.vn>
2026-03-28 01:18:48 +0700

    .

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,24 +2,35 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
-    use WithoutModelEvents;
-
     /**
      * Seed the application's database.
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        // 1. Tạo sẵn tài khoản Admin
+        DB::table('admins')->insert([
+            'email' => 'admin@gmail.com',
+            'password' => Hash::make('12345678'),
+            'full_name' => 'Sếp Tổng',
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        // 2. Tạo sẵn tài khoản User
+        DB::table('users')->insert([
+            'email' => 'user@gmail.com',
+            'password' => Hash::make('12345678'),
+            'full_name' => 'Nguyễn Công Thức',
+            'wallet_address' => '0xNguoiDungTest9876543210', 
+            'status' => 1,
+            'created_at' => now(),
+            'updated_at' => now()
         ]);
     }
 }
